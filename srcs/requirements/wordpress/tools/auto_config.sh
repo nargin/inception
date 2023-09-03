587b82100cbc03cf6e658ddf11a696e949a3f3c3
nrgn <mejumine@gmail.com>
2023-09-03 20:45:16 +0200

    to test : wordpress containers

---
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Auto config
+sleep 10
+
+wp config create	--allow-root \
+--dbname=$SQL_DATABASE \
+--dbuser=$SQL_USER \
+--dbpass=$SQL_PASSWORD \
+--dbhost=mariadb:3306 --path='/var/www/wordpress'
